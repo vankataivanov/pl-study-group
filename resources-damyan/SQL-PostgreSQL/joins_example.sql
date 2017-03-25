42f9254d4a81d738018ac18d63a16054b1ab648c
Damyan Damyanov <damyan.damyanov@scalefocus.com>
2017-03-25 14:13:50 +0000

    Added resources used for dagger and SQL Joins (Part 1 of addressed topics).

---
+++ resources-damyan/SQL-PostgreSQL/joins_example.sql
@@ -0,0 +1,45 @@
+﻿-- left join will select all records from the first table and 
+-- from the second table, only records that match the ID's, leaving 
+-- the columns from the second table empty on non matches
+SELECT * FROM EMPLOYEE e
+LEFT JOIN DEPARTMENT d
+ON e.DEP_ID = d.ID
+
+-- if we wan't to exclude the matching fields and use left join
+-- we add a where clause filtering the matching records.
+SELECT * FROM EMPLOYEE e
+LEFT JOIN DEPARTMENT d
+ON e.DEP_ID = d.ID
+WHERE e.DEP_ID IS NULL
+
+-- inner join will return only records that have matching ID's from both tables
+-- (just a JOIN will do the same)
+SELECT * FROM EMPLOYEE e
+INNER JOIN DEPARTMENT d
+ON e.DEP_ID = d.ID
+
+-- right join will select all matching records from the first table + all records from the second table,
+-- leaving empty columns on non matches.
+SELECT * FROM EMPLOYEE e
+RIGHT JOIN DEPARTMENT d
+ON e.DEP_ID = d.ID
+
+-- again filtering on nulls from the first table, we select all records from the second table that
+-- don't match any ID in the first table
+SELECT * FROM EMPLOYEE e
+RIGHT JOIN DEPARTMENT d
+ON e.DEP_ID = d.ID
+WHERE e.DEP_ID IS NULL
+
+-- full outer join will select everything from both tables and fill in nulls(empty records) in non matches.
+SELECT * FROM EMPLOYEE e
+FULL OUTER JOIN DEPARTMENT d
+ON e.DEP_ID = d.ID
+
+-- full outer join with filtering on non matches will return only rows from the first and second table
+-- that don't match on ID's, filling empty columns with NULL
+SELECT * FROM EMPLOYEE e
+FULL OUTER JOIN DEPARTMENT d
+ON e.DEP_ID = d.ID
+WHERE e.DEP_ID IS NULL
+OR d.ID IS NULL
---
+++ resources-damyan/dagger2/resources.md
@@ -0,0 +1,13 @@
+# Presentations:
+
+DAGGER 2 - A New Type of dependency injection:
+https://www.youtube.com/watch?v=oK_XtfXPkqw
+
+The Future of Dependency Injection with Dagger 2:
+https://www.youtube.com/watch?v=plK0zyRLIP8
+
+# Documentation/examples used:
+
+Coffee: https://google.github.io/dagger/users-guide.html
+Basic: https://medium.com/@Miqubel/understanding-dagger-2-367ff1bd184f#.xw0f4erra
+
